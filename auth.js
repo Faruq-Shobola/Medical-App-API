c30019565a92865e3be6bd9ff5a44bba268d87e9
shobola faruq <shobolafaruq@gmail.com>
2023-11-25 23:00:30 +0100

    Refactor

--- auth.js
+++ auth.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const admin = require("firebase-admin");
 
-const credentials = require(`./${process.env.FSERVICE}`);
+const credentials = JSON.parse(process.env.FSERVICE);
 
+console.log(credentials)
 admin.initializeApp({
   credential: admin.credential.cert(credentials),
 });
