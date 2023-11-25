f23e5219b56af54779a74f2eb0e7747f24f776f8
shobola faruq <shobolafaruq@gmail.com>
2023-11-25 23:10:43 +0100

    Refactor service file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./db/connect");
 const app = express();
 
@@ -12,6 +13,7 @@ const authenticateUser = require("./middleware/authentication");
 const dailyMedLogger = require("./cron");
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/v1/auth/", authRouter);
 app.use("/api/v1/medication/", authenticateUser, medicationRouter);
