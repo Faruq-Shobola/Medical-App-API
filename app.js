e77096e8c2ac117b93672ec41ff09b785c4e0cf5
shobola faruq <shobolafaruq@gmail.com>
2023-11-26 04:17:08 +0100

    Fix node version issue

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const authenticateUser = require("./middleware/authentication");
 const dailyMedLogger = require("./cron");
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use("/api/v1/auth/", authRouter);
 app.use("/api/v1/medication/", authenticateUser, medicationRouter);
--- auth.js
+++ auth.js
@@ -3,7 +3,6 @@ const admin = require("firebase-admin");
 
 const credentials = JSON.parse(process.env.FSERVICE);
 
-console.log(credentials)
 admin.initializeApp({
   credential: admin.credential.cert(credentials),
 });
